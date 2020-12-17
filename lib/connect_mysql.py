58dce4814bc9961f8206b72a8f6f79cca068a7df
xinjf <18884129577@163.com>
2020-12-17 13:04:56 +0800

    优化连接数据库的方式

--- lib/connect_mysql.py
+++ lib/connect_mysql.py
@@ -1,19 +1,25 @@
 import pymysql
+
+from lib.generate_logs import info
 from utils.operate_config import OperateIni
 from utils.settings import db_path
 
 
-def connect_mysql(db_name, sql):
+def connect_mysql(sql):
     """连接数据库
     ：:param
     db_name: 数据库名称
     sql： 需要执行的sql语句"""
-    config = OperateIni(db_path).ini_read_items("db.ini")
-    db = pymysql.connect(config["host"], config["user"], config["password"], db_name)
+    config = OperateIni("db.ini").ini_read_items(db_path)
+    db = pymysql.connect(config["host"], config["user"], config["password"])
     cursor = db.cursor()
-    cursor.execute(sql)
+    try:
+        cursor.execute(sql)
+    except Exception as e:
+        info("sql语句执行失败：{}".format(e))
     db.commit()
     data = cursor.fetchall()
     db.close()
     return data
 
+# print(connect_mysql("SELECT * from saas_iot.device"))
