02bbf44266effefb6475789a0c7b8ed9ca750ddb
xinjf <18884129577@163.com>
2020-12-30 19:14:19 +0800

    优化

---
+++ test_case/AssetManagement/common/test_firm.py
@@ -0,0 +1,42 @@
+import unittest
+from lib.operate_excel_data import OperateExcel
+from utils.dispose_params import deal_with_rely
+from utils.http_requests import http_requests
+from ddt import ddt,data
+from utils.login_set import LoginSet
+from lib.generate_logs import info
+
+excel_data = OperateExcel(r"\test_data\AssetManagement\common\common.xlsx", sheet_name="Firm").read_excel_data()
+
+@ddt
+class Test_Firm(unittest.TestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        cls.response = {}
+        cls.g = globals()
+        cls.g["token"] = LoginSet().get_token()
+
+    @data(*excel_data)
+    def test_01(self,item):
+        info("当前执行的测试用例是：{}".format(item["detail"]))
+
+        data = deal_with_rely(item["data"], self.response)
+
+        res = http_requests(url=item["url"], data=data, method=item["method"],token=self.g["token"])
+        self.response[item["case_id"]] = res
+
+        try:
+            self.assertEqual(item["check_result"]['code'], res["code"])
+            test_result = "PASS"
+        except AssertionError as e:
+            print("断言错误：{}".format(e))
+            test_result = "Fail"
+            raise e
+        finally:
+            OperateExcel(r"\test_data\AssetManagement\common\common.xlsx",
+                        sheet_name="Firm").write_excel_data(item["case_id"] + 1,str(res),test_result)
+
+
+if __name__ == '__main__':
+    unittest.main()
--- 接口测试.py
+++ 接口测试.py
@@ -7,10 +7,10 @@ from utils.ramdom_params import RandomParams
 
 
 
-for i in range(1,2):
-    token = "eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJleHAiOjE2MTE0Njc0ODksImRhdGEiOnsiaWQiOjEsIm1vYmlsZSI6IjE4ODg0MTI5NTc3IiwibmFtZSI6Ilx1OGQ4NVx1N2VhN1x1N2JhMVx1NzQwNlx1NTQ1OCIsInNleCI6MSwiZW1haWwiOiIxODg3Nzc3Nzc3Nzc3QDE2My5jb20iLCJwaG90b191cmwiOiJodHRwczpcL1wveGtkLXNhYXMtYmFzZS5vc3MtY24tYmVpamluZy5hbGl5dW5jcy5jb21cL3FhLXN0c3BcLzIwMjAxMjI0XC9kXC9PNGp4YlVJc3YzZGRzT2JCVVg5N28xMXo3Z2hRQjAwRFpZWWdoYmZTLmpwZyIsInJlbWFyayI6Inh4eHh4eHh4eHh4eHgiLCJjcmVhdGVkX2F0IjoiMCIsInVwZGF0ZWRfYXQiOiIxNjA4ODAwNjQxIiwiZGVsZXRlZF9hdCI6bnVsbH19.KYClVzHlNfQRFwnWZmcvwFM3_bKYb75vWbe7BNM7S_dCCwTihZT1l4CPwMaVLfbJe2uRZXp0S4pThsOnWAVji6zmDKw3oiPIVfZmnDgK_1U35bqpssj2lzycDuonXVdobomh-sGjDnrtVnnx4e3ckEQrUnzvqJFEGJw8imTazHU"
-    data = '{"face_img":"","name":"员工_${random_str}","mobile":${random_phone},"email":"","sex":1,"remark":""}'
-    url = "http://operator_backend.backend.qa-stsp.heroera.com/api/staff/store"
+for i in range(1,10):
+    token = "eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJleHAiOjE2MTE5MTQzNTMsImRhdGEiOnsiaWQiOjEsIm1vYmlsZSI6IjE4ODg0MTI5NTc3IiwibmFtZSI6Ilx1OGQ4NVx1N2VhN1x1N2JhMVx1NzQwNlx1NTQ1OCIsInNleCI6MiwiZW1haWwiOiIxMUAxNjMuY29tIiwicGhvdG9fdXJsIjoiaHR0cHM6XC9cL3hrZC1zYWFzLWJhc2Uub3NzLWNuLWJlaWppbmcuYWxpeXVuY3MuY29tXC9wcm9kLXVwbG9hZFwvMjAyMDEyMzBcLzNcL09MYXA3SGwzQ2FwVk9rQXljSm8yN1hFejVPWmJBNzM1ZE1hb0tJdXEuanBnIiwicmVtYXJrIjoieHh4IiwiY3JlYXRlZF9hdCI6IjE2MDkzMDc1NTYiLCJ1cGRhdGVkX2F0IjoiMTYwOTMyMjE4NCIsImRlbGV0ZWRfYXQiOm51bGx9fQ.EZvn4kReCM40SrVgmouWp7AxxY44gjAJVBEQL0fkM31OhGhnDGk79VwOKZU0pY_FOzQiRfdU5nDNyPEoFFUIZXHd5_3p_8iTrpVbvUsqpa1xfjtb93RwATi3G-1Ifrmxk2b95Y84JALSLc4tevimKfChdyC_J33J7UhJPFourJM"
+    data = '{"name":"员工_${random_str}","mobile":"${random_phone}","email":"","sex":1,"remark":"","photo_url":""}'
+    url = "http://operator_backend.backend.prod-stsp.heroera.com/api/staff/store"
     method = "post"
     data=RandomParams().build_random_params(data)
     data = json.loads(data)
