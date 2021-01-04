3e1199860194d1359a4913a3d15a1c4fcb3ba911
xinjf <18884129577@163.com>
2021-01-04 09:27:29 +0800

    2020.01.04优化

--- lib/operate_excel_data.py
+++ lib/operate_excel_data.py
@@ -18,28 +18,31 @@ class OperateExcel:
     def read_excel_data(self):
         wb = load_workbook(self.file_path)
         sheet = wb[self.sheet_name]
-        # print("sheet:{}".format(sheet))
         test_data = []
         for i in range(2, sheet.max_row + 1):
-            sub_data = {
-                "case_id": sheet.cell(i, 1).value,
-                "detail": sheet.cell(i, 2).value,
-                "method": sheet.cell(i, 3).value,
-                "header": sheet.cell(i, 4).value,
-                "url": sheet.cell(i, 5).value,
-                "data": sheet.cell(i, 6).value,
-                "sql": sheet.cell(i, 7).value,
-                "sql_check": sheet.cell(i, 8).value,
-                "check_result": sheet.cell(i, 9).value}
-            if sub_data["case_id"] is None:
-                break
-            else:
+            try:
+                sub_data = {
+                    "case_id": sheet.cell(i, 1).value,
+                    "detail": sheet.cell(i, 2).value,
+                    "method": sheet.cell(i, 3).value,
+                    "header": sheet.cell(i, 4).value,
+                    "url": sheet.cell(i, 5).value,
+                    "data": sheet.cell(i, 6).value,
+                    "sql": sheet.cell(i, 7).value,
+                    "sql_check": sheet.cell(i, 8).value,
+                    "check_result": sheet.cell(i, 9).value}
+                print(type(sub_data))
+                if sub_data["case_id"] is None:
+                    pass
+                else:
+                    sub_data["url"] = operator_url + sub_data["url"]
+                    sub_data["check_result"] = eval(sub_data["check_result"])
+                    sub_data["data"] = RandomParams().build_random_params(sub_data["data"])
+                    test_data.append(sub_data)
                 sub_data["url"] = operator_url + sub_data["url"]
-                sub_data["check_result"] = eval(sub_data["check_result"])
-                sub_data["data"] = RandomParams().build_random_params(sub_data["data"])
-                test_data.append(sub_data)
+            except ValueError as e:
+                info("读取excel的{0}的第{1}条时的参数有误:{2}".format(self.sheet_name,i,e))
 
-            # sub_data["url"] = operator_url + sub_data["url"]
         wb.close()  # 读取文件后需要关闭，否则会报无打开文件权限
         return test_data
 
@@ -55,3 +58,6 @@ class OperateExcel:
         wb = load_workbook(self.file_path)
         sheet_list = wb.sheetnames
         return sheet_list
+
+if __name__=="__main__":
+    OperateExcel(r"\test_data\AssetManagement\common\common.xlsx", sheet_name="Firm").read_excel_data()
--- 接口测试.py
+++ 接口测试.py
@@ -1,18 +1,21 @@
-import json
+# import json
+#
+# from utils.http_requests import http_requests
+# from utils.login_set import LoginSet
+# from utils.ramdom_params import RandomParams
+#
+#
+#
+#
+# for i in range(1,10):
+#     token = "eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJleHAiOjE2MTE5MTQzNTMsImRhdGEiOnsiaWQiOjEsIm1vYmlsZSI6IjE4ODg0MTI5NTc3IiwibmFtZSI6Ilx1OGQ4NVx1N2VhN1x1N2JhMVx1NzQwNlx1NTQ1OCIsInNleCI6MiwiZW1haWwiOiIxMUAxNjMuY29tIiwicGhvdG9fdXJsIjoiaHR0cHM6XC9cL3hrZC1zYWFzLWJhc2Uub3NzLWNuLWJlaWppbmcuYWxpeXVuY3MuY29tXC9wcm9kLXVwbG9hZFwvMjAyMDEyMzBcLzNcL09MYXA3SGwzQ2FwVk9rQXljSm8yN1hFejVPWmJBNzM1ZE1hb0tJdXEuanBnIiwicmVtYXJrIjoieHh4IiwiY3JlYXRlZF9hdCI6IjE2MDkzMDc1NTYiLCJ1cGRhdGVkX2F0IjoiMTYwOTMyMjE4NCIsImRlbGV0ZWRfYXQiOm51bGx9fQ.EZvn4kReCM40SrVgmouWp7AxxY44gjAJVBEQL0fkM31OhGhnDGk79VwOKZU0pY_FOzQiRfdU5nDNyPEoFFUIZXHd5_3p_8iTrpVbvUsqpa1xfjtb93RwATi3G-1Ifrmxk2b95Y84JALSLc4tevimKfChdyC_J33J7UhJPFourJM"
+#     data = '{"name":"员工_${random_str}","mobile":"${random_phone}","email":"","sex":1,"remark":"","photo_url":""}'
+#     url = "http://operator_backend.backend.prod-stsp.heroera.com/api/staff/store"
+#     method = "post"
+#     data=RandomParams().build_random_params(data)
+#     data = json.loads(data)
+#     res=http_requests(url,data =data,method=method,token=token)
+#     print(res)
 
-from utils.http_requests import http_requests
-from utils.login_set import LoginSet
-from utils.ramdom_params import RandomParams
 
 
-
-
-for i in range(1,10):
-    token = "eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJleHAiOjE2MTE5MTQzNTMsImRhdGEiOnsiaWQiOjEsIm1vYmlsZSI6IjE4ODg0MTI5NTc3IiwibmFtZSI6Ilx1OGQ4NVx1N2VhN1x1N2JhMVx1NzQwNlx1NTQ1OCIsInNleCI6MiwiZW1haWwiOiIxMUAxNjMuY29tIiwicGhvdG9fdXJsIjoiaHR0cHM6XC9cL3hrZC1zYWFzLWJhc2Uub3NzLWNuLWJlaWppbmcuYWxpeXVuY3MuY29tXC9wcm9kLXVwbG9hZFwvMjAyMDEyMzBcLzNcL09MYXA3SGwzQ2FwVk9rQXljSm8yN1hFejVPWmJBNzM1ZE1hb0tJdXEuanBnIiwicmVtYXJrIjoieHh4IiwiY3JlYXRlZF9hdCI6IjE2MDkzMDc1NTYiLCJ1cGRhdGVkX2F0IjoiMTYwOTMyMjE4NCIsImRlbGV0ZWRfYXQiOm51bGx9fQ.EZvn4kReCM40SrVgmouWp7AxxY44gjAJVBEQL0fkM31OhGhnDGk79VwOKZU0pY_FOzQiRfdU5nDNyPEoFFUIZXHd5_3p_8iTrpVbvUsqpa1xfjtb93RwATi3G-1Ifrmxk2b95Y84JALSLc4tevimKfChdyC_J33J7UhJPFourJM"
-    data = '{"name":"员工_${random_str}","mobile":"${random_phone}","email":"","sex":1,"remark":"","photo_url":""}'
-    url = "http://operator_backend.backend.prod-stsp.heroera.com/api/staff/store"
-    method = "post"
-    data=RandomParams().build_random_params(data)
-    data = json.loads(data)
-    res=http_requests(url,data =data,method=method,token=token)
-    print(res)
