b5d466ce431c4e2d2a0e21b3b8d740c6a4f5fe3f
xinjf <18884129577@163.com>
2021-01-13 17:24:23 +0800

    优化case

--- 接口测试.py
+++ 接口测试.py
@@ -1,20 +1,19 @@
-# import json
-#
-# from utils.http_requests import http_requests
-# from utils.login_set import LoginSet
-# from utils.ramdom_params import RandomParams
-#
-#
-#
-#
-# for i in range(1,10):
-#     token = "eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJleHAiOjE2MTE5MTQzNTMsImRhdGEiOnsiaWQiOjEsIm1vYmlsZSI6IjE4ODg0MTI5NTc3IiwibmFtZSI6Ilx1OGQ4NVx1N2VhN1x1N2JhMVx1NzQwNlx1NTQ1OCIsInNleCI6MiwiZW1haWwiOiIxMUAxNjMuY29tIiwicGhvdG9fdXJsIjoiaHR0cHM6XC9cL3hrZC1zYWFzLWJhc2Uub3NzLWNuLWJlaWppbmcuYWxpeXVuY3MuY29tXC9wcm9kLXVwbG9hZFwvMjAyMDEyMzBcLzNcL09MYXA3SGwzQ2FwVk9rQXljSm8yN1hFejVPWmJBNzM1ZE1hb0tJdXEuanBnIiwicmVtYXJrIjoieHh4IiwiY3JlYXRlZF9hdCI6IjE2MDkzMDc1NTYiLCJ1cGRhdGVkX2F0IjoiMTYwOTMyMjE4NCIsImRlbGV0ZWRfYXQiOm51bGx9fQ.EZvn4kReCM40SrVgmouWp7AxxY44gjAJVBEQL0fkM31OhGhnDGk79VwOKZU0pY_FOzQiRfdU5nDNyPEoFFUIZXHd5_3p_8iTrpVbvUsqpa1xfjtb93RwATi3G-1Ifrmxk2b95Y84JALSLc4tevimKfChdyC_J33J7UhJPFourJM"
-#     data = '{"name":"员工_${random_str}","mobile":"${random_phone}","email":"","sex":1,"remark":"","photo_url":""}'
-#     url = "http://operator_backend.backend.prod-stsp.heroera.com/api/staff/store"
-#     method = "post"
-#     data=RandomParams().build_random_params(data)
-#     data = json.loads(data)
-#     res=http_requests(url,data =data,method=method,token=token)
-#     print(res)
+import json
+from utils.http_requests import http_requests
+from utils.login_set import LoginSet
+from utils.ramdom_params import RandomParams
+
+
+
+
+for i in range(1,5):
+    token = "bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE2MTMwOTU2NDksImFkbWluX2lkIjoxLCJhZG1pbl9uYW1lIjoiYWRtaW4ifQ.AHxJE26Eg0xWIa56drjgGJ1Cd2hQs94fTJ0UIgXWle0"
+    data = {"carrier_name":"载体7","build_area":100,"carrier_province_code":120000000000,"carrier_city_code":120100000000,"carrier_district_code":120101000000,"carrier_address":"222","open_time":1610467200,"establish_time":1610467200,"operating_company":[],"unit_nature":2,"brand":"","carrier_desc":"","price_range_min":100,"price_range_max":200,"price_range_unit":1,"pics":"[{\"name\":\"u=3718703482,1601075361&fm=26&gp=0.jpg\",\"url\":\"https://xkd-saas-base.oss-cn-beijing.aliyuncs.com/qa/20210113/8/xmneVCWVclLG750SHYa9jGUgIfJ4Fm7p5ZTmbVja.jpg\",\"uid\":1610503691605,\"status\":\"success\"}]","id":13,"base_id":13}
+    url = "http://data-manage.backend.qa-saas.heroera.com/api/carrier/base/create"
+    method = "post"
+    data=RandomParams().build_random_params(data)
+    print(data)
+    res=http_requests(url,data =data,method=method,token=token)
+    print(res)
 
 
